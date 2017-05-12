7d42130f461654c06fedfb25f3fd3dc7f55f2a28
ShanJiang_Chorus <shan.jiang@chorus.se>
2017-05-12 15:52:46 +0200

    Start Generics chapter

---
+++ Swift3.1_Study/23_Generics.swift
@@ -0,0 +1,14 @@
+//
+//  23_Generics.swift
+//  Swift3.1_Study
+//
+//  Created by shan jiang on 2017-05-12.
+//  Copyright © 2017 shan jiang. All rights reserved.
+//
+
+import Foundation
+
+class Generics{
+    //class func
+    
+}
