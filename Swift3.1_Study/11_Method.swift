fdba7c8582b1bad5d81bdc97da99d8d5044397db
ShanJiang_Chorus <shan.jiang@chorus.se>
2017-06-07 12:00:55 +0200

    Start Method class

---
+++ Swift3.1_Study/11_Method.swift
@@ -0,0 +1,16 @@
+//
+//  11_Method.swift
+//  Swift3.1_Study
+//
+//  Created by shan jiang on 2017-06-07.
+//  Copyright © 2017 shan jiang. All rights reserved.
+//
+
+import Foundation
+class Method{
+    class func instanceMethod(){
+        /*----------------- 1. Instance Methods -----------------*/
+        print("/*----------------- 1. Instance Methods -----------------*/ \n")
+    }
+    
+}
